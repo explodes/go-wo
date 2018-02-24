ebfbc8759ad98fb5427da9b0675c74e15c2d9686
Evan Leis <evan.explodes@gmail.com>
2018-02-24 01:29:51 -0800

    [cleanup] unused

--- wobj/interface.go
+++
@@ -1,9 +0,0 @@
-package wobj
-
-import "github.com/faiface/pixel"
-
-// Bounder is an interface for something that has a hit box
-type Bounder interface {
-	// Bounds returns the Rect that encompasses this object
-	Bounds() pixel.Rect
-}
